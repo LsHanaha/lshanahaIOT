ae9b2fefaf74766e8bf429359f792e9e244b2b83
LsHanaha <kirik193@yandex.ru>
2022-06-26 19:37:43 +0200

    add second version

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ async def hello(request: fastapi.Request):
     return fastapi.Response("Hello world!")
 
 
+@app.route("/bye")
+async def bye(request: fastapi.Request):
+    return fastapi.Response("Bye world!")
+
+
 if __name__ == '__main__':
     uvicorn.run("main:app", port=18881, reload=True, debug=True)
